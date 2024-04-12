02e3821d2c5d4b58a446d97e6236c46f06daf892
jteo0 <jjtheodora@gmail.com>
2024-04-12 19:02:23 +0700

    Create Subscriber database and Subscriber repository struct skeleton.

---
+++ src/repository/subscriber.rs
@@ -0,0 +1,13 @@
+use dashmap::DashMap;
+use lazy_static::lazy_static;
+use crate::model::subscriber::Subscriber;
+
+// Singleton of Database
+lazy_static! {
+    static ref SUBSCRIBERS: DashMap<String, DashMap<String, Subscriber>> = DashMap::
+}
+
+pub struct SubscriberRepository;
+
+impl SubscriberRepository {
+}
